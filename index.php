25c9cd87b9a405fd1a8aca2a1558e3f987ec2a64
Pedro Henrique <seuemail@gmail.com>
2026-05-17 00:32:54 -0300

    4 commit

--- index.php
+++ index.php
@@ -5,35 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Saúde Animal Agro e Vet</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="#">Hidden brand</a>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+    <nav class="navbar-dark">
+  <div class="container-fluid d-flex justify-content-between align-items-center">
+
+    <a class="logo ms-5" href="index.php">
+      <img src="img/lojaicone.jpeg" alt="icone" class="logo-img">
+    </a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
   </div>
 </nav>
-<header>
+</header>
 <body>
-    
+    <main>
+        <div class="container mt-5">
+            <h1>Bem-vindo à Saúde Animal Agro e Vet</h1>
+            <p>Encontre os melhores produtos para o cuidado dos seus animais de estimação e para a agricultura.</p>
+        </div>
+        <div class="container-fluid p-0">
+          <div class="row g-0"
+            <div class="col-12">
+              <img src="img/banner.png" alt="banner" class="img-fluid w-100">
+            </div>
+          </div>
+        </div>
+      </main>
 </body>
 </html>
