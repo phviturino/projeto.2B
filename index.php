2d4fd61377818e5371ce417bb2e86cf63ca1de56
Pedro Henrique <seuemail@gmail.com>
2026-05-18 22:56:09 -0300

    4 commit

--- index.php
+++ index.php
@@ -6,29 +6,51 @@
     <title>Saúde Animal Agro e Vet</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
+<body>
 <header>
     <nav class="navbar-dark">
-  <div class="container-fluid d-flex justify-content-between align-items-center">
+  <div class="container-fluid"> 
 
-    <a class="logo ms-5" href="index.php">
+    <div class="row align-items-center"> 
+
+    <div class="col-md-3 col-sm-4">
+    <a class="logo" href="index.php">
       <img src="img/lojaicone.jpeg" alt="icone" class="logo-img">
     </a>
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+</div>
+
+<div class="col-md-6 col-sm-8">
+    <form class="d-flex busca" role="search">
+      <input class="form-control me-2 w-100" type="search" placeholder="Search" aria-label="Search"/>
       <button class="btn btn-outline-success" type="submit">Search</button>
     </form>
+    </div>
+
+    <div class="col-md-3 d-none d-md-block text-white text-end">
+      <div class="d-inline-flex aling-items-center info-contato">
+        <div class="contato me-2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+  <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+</svg>
+        </div>
+        <div class="d-flex flex-column">
+        <span class="d-block">Atendimento</span>
+        <span class="d-block">11 1234-5678</span>
+</div>
+  </div>
   </div>
 </nav>
 </header>
-<body>
+
     <main>
         <div class="container mt-5">
             <h1>Bem-vindo à Saúde Animal Agro e Vet</h1>
             <p>Encontre os melhores produtos para o cuidado dos seus animais de estimação e para a agricultura.</p>
         </div>
         <div class="container-fluid p-0">
-          <div class="row g-0"
+          <div class="row g-0">
             <div class="col-12">
               <img src="img/banner.png" alt="banner" class="img-fluid w-100">
             </div>
