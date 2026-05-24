70a536838197b711c633a24454d4701424ee27be
Pedro Henrique <seuemail@gmail.com>
2026-05-24 16:53:01 -0300

    footerfinal

--- includes/footer.php
+++ includes/footer.php
@@ -1,49 +1,77 @@
 <footer class="bg-dark text-white pt-5 pb-3">
     <div class="container">
 
-    <div class="row mb-4">
+        <!--logo + descrição-->
+    <div class="row align-itens-center mb-3">
+    <div class="col-md-3">
+    <img src="img/lojaicone.jpeg" alt="icone" class="logo-footer rounded-circle w-100">
+    </div>
+    <div class="col-md-10">
+        <p class="text-secondary">Sua loja completa, dedicada ao cuidado, desde o seu pet até sua fazenda. 4 lojas oferecendo o melhor a nossos clientes.</p>
+    </div>
+    </div>
+    
+    <hr class="border-secondary">
 
-    <div class="col-md-4 mb-4">
-    <img src="img/lojaicone.jpeg" alt="icone" class="logo-footer">
-    <p class="descricao">Sua loja completa, dedicada ao cuidado, desde o seu pet até sua fazenda. 4 lojas oferecendo o melhor a nossos clientes.</p>
+    <!--lojas-->
+    <div class="col-6 col-md-3 mb-4">
+        <div class="col-6 col-md-3 mb-4">
+        <h6 class="text-warning text-uppercase fw-bold">Boa Esperança</h6>
+        <p class="text-secondary small">Rua Bahia, 354 - Centro, Boa Esperança - PR</p>
+        <a href="contato.php" class="text-success text-decoration-none">
+        <i class="bi bi-whatsapp"></i>Whatsapp
+        </a>
+        </div>
 
-    <div class="loja-card">
-        <h2>Boa Esperança</h2>
-        <p>Rua Bahia, 354 - Centro, Boa Esperança - PR</p>
-        <a href="contato.php" class="btn-contato">
-            <i class="bi bi-whatsapp"></i>
+        <div class="col-6 col-md-3 mb-4">
+        <h6 class="text-warning text-uppercase fw-bold">Boa Esperança</h6>
+        <p class="text-secondary small">Rua Bahia, 354 - Centro, Boa Esperança - PR</p>
+        <a href="contato.php" class="text-success text-decoration-none">
+        <i class="bi bi-whatsapp"></i>Whatsapp
         </a>
-    </div>
-    <div class="loja-card">
-        <h2>Rancho Alegre do Oeste</h2>
-        <p>Rua 5, 54 - Centro, Rancho Alegre do Oeste - PR</p>
-        <a href="contato.php" class="btn-contato">
-            <i class="bi bi-whatsapp"></i>
+        </div>
+
+        <div class="col-6 col-md-3 mb-4">
+        <h6 class="text-warning text-uppercase fw-bold">Boa Esperança</h6>
+        <p class="text-secondary small">Rua Bahia, 354 - Centro, Boa Esperança - PR</p>
+        <a href="contato.php" class="text-success text-decoration-none">
+        <i class="bi bi-whatsapp"></i>Whatsapp
         </a>
-    </div>
-    <div class="loja-card">
-        <h2>Goioerê</h2>
-        <p>Rua Amazonas, 71 - Centro, Goioerê - PR</p>
-        <a href="contato.php" class="btn-contato">
-            <i class="bi bi-whatsapp"></i>
+        </div>
+
+        <div class="col-6 col-md-3 mb-4">
+        <h6 class="text-warning text-uppercase fw-bold">Boa Esperança</h6>
+        <p class="text-secondary small">Rua Bahia, 354 - Centro, Boa Esperança - PR</p>
+        <a href="contato.php" class="text-success text-decoration-none">
+        <i class="bi bi-whatsapp"></i>Whatsapp
         </a>
+        </div>
+
     </div>
-    <div class="loja-card">
-        <h2>Quarto Centenário</h2>
-        <p>Rua Acre, 34 - Centro, Quarto Centenário - PR</p>
-        <a href="contato.php" class="btn-contato">
-            <i class="bi bi-whatsapp"></i>
-        </a>
+
+    <hr class="border-secondary">
+
+    <!--rodape final-->
+    <div class="d-flex justify-content-between align-itens-center py-3">
+        <p>Saúde Animal Agro e Vet - Todos os direitos reservados</p>
     </div>
-    <p>Saúde Animal Agro e Vet - Todos os direitos reservados</p>
-    <div class="instagram">
+    </div>
+
+    <div class="col-md-8">
+        <div class="row">
+
+        
+
+        
+        <div class="instagram">
         <i class="bi bi-instagram"></i>
         <a href="https://www.instagram.com/saudeanimalagroevet/"></a>
+        </div>
+    </div>
     </div>
     <btn>
         <a href="#" class="sobre">Saiba mais sobre nós</a>
     </btn>
-</div>
 </footer>
 </body>
 </html>
