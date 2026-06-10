d296f94b16a11aa9b9688d896ed33c787cfe5a39
Pedro Henrique <seuemail@gmail.com>
2026-06-10 20:28:38 -0300

    catalogo

--- produtos.php
+++ produtos.php
@@ -37,33 +37,49 @@
     
     <div class="row row-cols-1 row-cols-md-4 g-4">
         
-        <?php foreach ($produto as $produto): ?>
-            <div class="col">
-                <div class="card h-100 shadow border-0 bg-dark text-white">
-                    
-                    <div class="p-3 bg-white d-flex align-items-center justify-content-center container-foto-produto">
-                        <img src="img/<?php echo $produto['imagem']; ?>" class="img-fluid foto-produto" alt="<?php echo $produto['nome']; ?>">
-                    </div>
-                    
-                    <div class="card-body d-flex flex-column text-center">
-                        <h5 class="card-title fs-6 text-uppercase mb-2 nome-produto">
-                            <?php echo $produto['nome']; ?>
-                        </h5>
-                        
-                        <p class="card-text text-secondary small mb-3">
-                            <?php echo $produto['categoria']; ?>
-                        </p>
-                        
-                        <p class="card-text font-weight-bold fs-5 mt-auto mb-2 preco-produto">
-                            R$ <?php echo number_format($produto['preco'], 2, ',', '.'); ?>
-                        </p>
-                        
-                        <a href="produto-detalhes.php?id=<?php echo $produto['id']; ?>" class="btn btn-outline-light btn-sm w-100">Ver produto</a>
+        <?php 
+        
+        if (empty($produto)): 
+        ?>
+            <div class="col-12 text-center my-5">
+                <p class="text-muted">Nenhum produto cadastrado encontrado.</p>
+            </div>
+
+        <?php 
+        
+        else: 
+            foreach ($produto as $linha_produto): 
+        ?>
+                <div class="col">
+                    <div class="card h-100 shadow border-0 bg-dark text-white">
+
+                        <div class="p-3 bg-white d-flex align-items-center justify-content-center container-foto-produto">
+                            <img src="img/<?php echo $linha_produto['imagem']; ?>" class="img-fluid foto-produto" alt="<?php echo $linha_produto['nome']; ?>">
+                        </div>
+
+                        <div class="card-body d-flex flex-column text-center">
+                            <h5 class="card-title fs-6 text-uppercase mb-2 nome-produto">
+                                <?php echo $linha_produto['nome']; ?>
+                            </h5>
+
+                            <p class="card-text text-secondary small mb-3">
+                                Categoria #<?php echo $linha_produto['id_categoria']; ?>
+                            </p>
+
+                            <p class="card-text font-weight-bold fs-5 mt-auto mb-2 preco-produto">
+                                R$ <?php echo number_format($linha_produto['preco'], 2, ',', '.'); ?>
+                            </p>
+
+                            <a href="produto-detalhes.php?id=<?php echo $linha_produto['id']; ?>" class="btn btn-outline-light btn-sm w-100">Ver Produtos</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php endforeach; ?>
-
+        <?php 
+        
+            endforeach; 
+        endif; 
+        ?>
+        
     </div>
 </main>
 
