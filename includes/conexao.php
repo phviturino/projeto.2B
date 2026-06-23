afeb19e1cee05f5e5fabb1631e437f5b78b9f6e8
Pedro Henrique <seuemail@gmail.com>
2026-06-23 00:29:14 -0300

    update final

--- includes/conexao.php
+++ includes/conexao.php
@@ -6,6 +6,8 @@ $dbname = "ezyro_42246583_saudeanimal";
 
 $conexao = new mysqli($servername, $username, $password, $dbname);
 
+$conexao->set_charset("utf8mb4");
+
 if ($conexao->connect_error) {
     die("Falha na conexão: " . $conexao->connect_error);
 }
