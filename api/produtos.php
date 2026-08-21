5a0ff52a5eab947de90d612b18ce664c80194f3e
Pedro Henrique <seuemail@gmail.com>
2026-08-20 21:41:29 -0300

    subindo api

---
+++ api/produtos.php
@@ -0,0 +1,16 @@
+<?php
+    require_once __DIR__ . '/../includes/conexao.php';
+
+    header('Content-Type: application/json; charset=utf-8');
+
+    $sql = "SELECT * FROM produto";
+    $resultado = mysqli_query($conexao, $sql);
+    $produto = array();
+
+    while ($linha = mysqli_fetch_assoc($resultado)) {
+        $produto[] = $linha;
+    }
+
+    echo json_encode($produto)
+
+?>
